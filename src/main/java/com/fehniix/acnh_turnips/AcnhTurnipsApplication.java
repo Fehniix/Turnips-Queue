f4be6487441eb2f0dbc37cc17466ec054a9f9985
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-13 21:23:01 +0100

    Updated project with initializer.

--- src/main/java/com/fehniix/acnh_turnips/AcnhTurnipsApplication.java
+++ src/main/java/com/fehniix/acnh_turnips/AcnhTurnipsApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.fehniix.acnh_turnips;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AcnhTurnipsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AcnhTurnipsApplication.class, args);
 	}
 
 }
--- src/test/java/com/fehniix/acnh_turnips/AcnhTurnipsApplicationTests.java
+++ src/test/java/com/fehniix/acnh_turnips/AcnhTurnipsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.fehniix.acnh_turnips;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AcnhTurnipsApplicationTests {
 
 	@Test
 	void contextLoads() {
