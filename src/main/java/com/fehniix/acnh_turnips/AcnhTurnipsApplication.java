331d5ddce933d58faa29409385f35616bf19b61d
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-14 17:07:50 +0100

    Added log.

--- src/main/java/com/fehniix/acnh_turnips/AcnhTurnipsApplication.java
+++ src/main/java/com/fehniix/acnh_turnips/AcnhTurnipsApplication.java
@@ -1,12 +1,14 @@
 package com.fehniix.acnh_turnips;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ansi.AnsiColor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcnhTurnipsApplication {
 
 	public static void main(String[] args) {
+		Logger.log(AnsiColor.CYAN, "\n\nApplication started.\n\n");
 		SpringApplication.run(AcnhTurnipsApplication.class, args);
 	}
 
