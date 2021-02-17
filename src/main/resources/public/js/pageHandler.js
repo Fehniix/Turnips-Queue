e71dd4e11fb22c68e051b68b81c85f188442f9f7
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-17 15:46:29 +0100

    First commit

--- src/main/resources/public/js/pageHandler.js
+++ src/main/resources/public/js/pageHandler.js
@@ -1,14 +1,51 @@
+import Animator from "./animator.js";
+
 class PageHandler {
 	constructor() {
+		// Each value contains the reference to each DOM object. Keys correspond to the "pageName" attribute of the pages.
+		this.pages = {
+			main: undefined,
+			host: undefined,
+			hostedIsland: undefined,
+			islands: undefined,
+		};
 
+		this.activePageName = undefined;
 	}
 
+	/**
+	 * Retrieves the HTML code from the DOM of the different pages to swap between, and initializes page instances.
+	 */
 	initialize() {
+		$('.page').each((_, pageElement) => {
+			const pageName = $(pageElement).attr('pageName');
+
+			this.pages[pageName] = pageElement;
+		});
+
+		//	First page to be shown is the main page.
+		this.activePageName = 'main';
 		
+		// After initialization, make the pages object strictly read-only. Nothing needs to change.
+		Object.freeze(this.pages);
 	}
 
-	swapPage(pageId) {
+	/**
+	 * Swaps to the page identified by its unique name.
+	 */
+	async swapToPage(pageName) {
+		await Animator.slideOff(this.pages[this.activePageName]);
+		await Animator.slideOn(this.pages[pageName]);
+		
+		//	Set the currently active page.
+		this.activePageName = pageName;
+	}
 
+	/**
+	 * Returns the currently active page name.
+	 */
+	getActivePageName() {
+		return this.activePageName;
 	}
 }
 
