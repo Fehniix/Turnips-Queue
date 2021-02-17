c54921f6a1178b9a53fd54a9b953fbbc5301359a
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-17 15:46:20 +0100

    First commit

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,8 +1,37 @@
 'use strict';
 
-// Main, and strictly anonoymous, entry point.
+import PageHandler from './pageHandler.js';
+import Animator from './animator.js';
+
+//	Main, and strictly anonoymous, entry point.
 (() => {
 	window.addEventListener('load', () => {
+		//	Initialize the Page Handler.
+		PageHandler.initialize();
+
+		//	Event binding.
 		
+		//	Main page.
+		$('#noteHost').on('click', _ => {
+			PageHandler.swapToPage('host');
+		});
+
+		//	Host page.
+		$('.hostPage .btnCancel').on('click', _ => {
+			PageHandler.swapToPage('main');
+		});
+
+		//	Modals
+		$('#linkRules').on('click', _ => {
+			Animator.showModal('modalRules');
+		});
+
+		$('#linkFAQ').on('click', _ => {
+			Animator.showModal('modalFAQ');
+		});
+
+		$('.blackOverlay').on('click', _ => {
+			Animator.hideModal();
+		});
 	});
 })();
