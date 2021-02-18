6da1e0849de29d0d77bfeadb2caea58dfcf95707
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-18 17:17:30 +0100

    Added individual page imports for code inj.

--- src/main/resources/public/js/pageHandler.js
+++ src/main/resources/public/js/pageHandler.js
@@ -1,5 +1,11 @@
 import Animator from "./animator.js";
 
+//	Taking advantage of ES module loading, each constructor is executed as an entrypoint for code injection for the purpose of tidying and code refactoring.
+import _main from './pages/main.js';
+import _host from './pages/host.js';
+import _island from './pages/island.js';
+import _islands from './pages/islands.js';
+
 class PageHandler {
 	constructor() {
 		// Each value contains the reference to each DOM object. Keys correspond to the "pageName" attribute of the pages.
@@ -26,12 +32,12 @@ class PageHandler {
 		//	First page to be shown is the main page.
 		this.activePageName = 'main';
 		
-		// After initialization, make the pages object strictly read-only. Nothing needs to change.
+		// 	After initialization, make the pages object strictly read-only. Nothing needs to change.
 		Object.freeze(this.pages);
 	}
 
 	/**
-	 * Swaps to the page identified by its unique name.
+	 * Swaps to the page identified by its unique name. The `flex` parameter specifies the target page is a FlexBox item.
 	 */
 	async swapToPage(pageName, flex = false) {
 		await Animator.slideOff(this.pages[this.activePageName]);
@@ -39,6 +45,9 @@ class PageHandler {
 		
 		//	Set the currently active page.
 		this.activePageName = pageName;
+
+		//	Trigger custom pageSwapped event.
+		$(window).trigger('pageSwapped');
 	}
 
 	/**
