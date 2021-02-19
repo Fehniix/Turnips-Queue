04e7253875905b4985b121da645b1eecbb3a7c9e
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-19 20:43:42 +0100

    Added testing socket support.

--- src/main/java/com/fehniix/acnh_turnips/routers/RouterREST.java
+++ src/main/java/com/fehniix/acnh_turnips/routers/RouterREST.java
@@ -4,16 +4,26 @@ import java.util.ArrayList;
 
 import com.fehniix.acnh_turnips.FileIO;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RouterREST {
 
+	@Autowired
+	private SimpMessagingTemplate simpMessagingTemplate;
+
 	@GetMapping("/pagesFolderContents")
 	public final String[] pagesFolderContents() {
 		ArrayList<String> contents = new ArrayList<String>(FileIO.getFileList("./src/main/resources/public/js/pages"));
 		return contents.toArray(new String[0]);
+	}
+
+	@GetMapping("/test")
+	public final void test() {
+		this.simpMessagingTemplate.convertAndSend("/topic/queue", "hello from Java!");
 	}
 
 }
