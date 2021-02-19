83424ccf4d5ef38f46d66f1bc51d42d57891a6a3
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-19 18:30:32 +0100

    External music API!

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -4,9 +4,14 @@ import PageHandler from './pageHandler.js';
 import PageRouter from './pageRouter.js';
 import Animator from './animator.js';
 import Extensions from './extensions.js';
+import { Base64 } from 'https://cdn.jsdelivr.net/npm/js-base64@3.6.0/base64.mjs';
 
 //	Main, and strictly anonoymous, entry point.
 (() => {
+	//	Audio object used to control random music from ACNH API.
+	let audio = undefined;
+	let audioPlaying = false;
+
 	window.addEventListener('load', () => {
 		//	Load extensions first.
 		Extensions();
@@ -16,6 +21,9 @@ import Extensions from './extensions.js';
 
 		//	Load pages dynamically.
 		PageRouter.loadPages(PageHandler);
+
+		//	Load random music!
+		loadMusic();
 
 		//	Global binds.
 
@@ -41,4 +49,34 @@ import Extensions from './extensions.js';
 			Animator.hideModal();
 		});
 	});
+
+	/**
+	 * Loads random music from the ACNH API website that freely hosts ACNH data.
+	 */
+	async function loadMusic() {
+		const hourlyMusic 		= await $.get('http://acnhapi.com/v1/backgroundmusic/');
+		const hourlyMusicKeys 	= Object.keys(hourlyMusic);
+		const randomTrack 		= hourlyMusic[hourlyMusicKeys[Math.floor(Math.random() * hourlyMusicKeys.length)]];
+		
+		audio = new Audio(randomTrack.music_uri);
+
+		//	Additionally bind and handle play/pause events.
+		$('.audio').on('click', _ => {
+			toggleMusic();
+		});
+	}
+
+	function toggleMusic() {
+		if (audioPlaying === true) {
+			$('.audio .play').show();
+			$('.audio .pause').hide();
+			audio.pause();
+		} else {
+			$('.audio .play').hide();
+			$('.audio .pause').show();
+			audio.play();
+		}
+
+		audioPlaying = !audioPlaying;
+	}
 })();
