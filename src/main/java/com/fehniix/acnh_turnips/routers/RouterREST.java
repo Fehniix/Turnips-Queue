f7317836c42f938506d958b7a194c7ebdb70e77b
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-19 15:24:47 +0100

    Added pagesFolderContent entrypoint

--- src/main/java/com/fehniix/acnh_turnips/routers/RouterREST.java
+++ src/main/java/com/fehniix/acnh_turnips/routers/RouterREST.java
@@ -1,5 +1,19 @@
 package com.fehniix.acnh_turnips.routers;
 
+import java.util.ArrayList;
+
+import com.fehniix.acnh_turnips.FileIO;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RouterREST {
-	
+
+	@GetMapping("/pagesFolderContents")
+	public final String[] pagesFolderContents() {
+		ArrayList<String> contents = new ArrayList<String>(FileIO.getFileList("./src/main/resources/public/js/pages"));
+		return contents.toArray(new String[0]);
+	}
+
 }
