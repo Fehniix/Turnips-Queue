f794c9b537ba5f1cbf29fb0e8e935270da0426b7
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-19 15:26:23 +0100

    Abstracted event handling to single modules.

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,60 +1,29 @@
 'use strict';
 
 import PageHandler from './pageHandler.js';
+import PageRouter from './pageRouter.js';
 import Animator from './animator.js';
+import Extensions from './extensions.js';
 
 //	Main, and strictly anonoymous, entry point.
 (() => {
 	window.addEventListener('load', () => {
-		//	Inject to jQuery's prototype a method to show FlexBox items.
-		jQuery.prototype.showFlex = function() {
-			this.css('display', 'flex');
-		};
+		//	Load extensions first.
+		Extensions();
 
 		//	Initialize the Page Handler.
 		PageHandler.initialize();
 
-		//	Event binding.
+		//	Load pages dynamically.
+		PageRouter.loadPages(PageHandler);
 
+		//	Global binds.
 		$(window).on('islandsLoaded', _ => {
 			$('.island').each((index, element) => {
 				//	Rotate island notes randomly.
 				console.log(element)
 			});
 		});
-		
-		//	Main page.
-		$('#noteHost').on('click', _ => {
-			PageHandler.swapToPage('host', true);
-		});
-
-		$('#noteJoin').on('click', _ => {
-			PageHandler.swapToPage('island', true);
-		});
-
-		$('#noteIslands').on('click', _ => {
-			PageHandler.swapToPage('islands', true);
-		});
-
-		//	Host page.
-		$('.hostPage .btnCancel').on('click', _ => {
-			PageHandler.swapToPage('main');
-		});
-
-		$('.hostPage .btnNext').on('click', _ => {
-			PageHandler.swapToPage('hostStep2', true);
-		});
-
-		$('.hostPageStep2 .btnBack').on('click', _ => {
-			PageHandler.swapToPage('host', true);
-		});
-
-		//	Island page
-		$('.islandPage .btnBack').on('click', _ => {
-			PageHandler.swapToPage('main');
-		});
-
-		// Islands page
 
 		//	Modals
 		$('#linkRules').on('click', _ => {
