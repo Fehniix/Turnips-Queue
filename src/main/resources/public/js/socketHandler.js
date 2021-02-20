0a11e9338b59e03932478553e8e342be63b36b5d
Fehniix <29045904+Fehniix@users.noreply.github.com>
2021-02-20 11:12:50 +0100

    Disabled Stomp debug logs.

--- src/main/resources/public/js/socketHandler.js
+++ src/main/resources/public/js/socketHandler.js
@@ -12,8 +12,10 @@ class SocketHandler {
 	async initialize() {
 		this.socketClient = new SockJS('/ws-fallback');
 		this.stompClient = Stomp.over(this.socketClient);
+		this.stompClient.debug = null;
 
 		await this.stompClient.connect({}, frame => {
+			console.log('Connected to the WS server endpoint.');
 			this.stompClient.subscribe('/topic/queue', data => {
 				this.messageReceived(data);
 			});
